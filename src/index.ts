a278fd7c1f28fdfff68fdba5b708a5e1e3bbe0e2
gusmgoes <ggoes269@gmail.com>
2023-04-10 01:12:03 -0400

    :books: docs: adding project swagger documentation

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,17 @@ import * as dotenv from 'dotenv';
 import routeCar from './routes/routeCar';
 import routerUser from './routes/routeUser';
 import routerReservation from './routes/routeReservations';
+import swaggerUi from 'swagger-ui-express';
 dotenv.config();
 
 const app = express();
+import swaggerDocument from './documentation/swaggerdocs.json';
 
 app.use(express.json());
 app.use('/api/v1', routeCar);
 app.use('/api/v1', routerUser);
 app.use('/api/v1', routerReservation);
+app.use('/api/v1/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 app.listen(parseInt(process.env.PORT!), () => {
